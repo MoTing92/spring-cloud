e7a80cf8b0f0d3901f2436081f8d51069633c630
moting <18209202332@163.com>
2017-10-31 22:45:24 +0800

    添加路由过滤器，实现了过滤（可以进入到过滤器），但是无法获取accessToken参数值，即使已经传过去。验证尚未通过。

--- api-gateway/src/main/java/com/moting/spring/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/moting/spring/ApiGatewayApplication.java
@@ -3,6 +3,9 @@ package com.moting.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.moting.spring.filter.AccessFilter;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -10,5 +13,10 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	
+	@Bean
+	public AccessFilter accessFilter(){
+		return new AccessFilter();
 	}
 }
