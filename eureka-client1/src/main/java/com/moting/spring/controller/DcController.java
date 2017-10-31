dbb79c968775988ab20b2f75df4dc8438b811eb9
moting <18209202332@163.com>
2017-10-31 16:37:52 +0800

    将客户端1的请求延时5s响应，用于验证hystrix降级保护机制。

--- eureka-client1/src/main/java/com/moting/spring/controller/DcController.java
+++ eureka-client1/src/main/java/com/moting/spring/controller/DcController.java
@@ -12,7 +12,8 @@ public class DcController {
     DiscoveryClient discoveryClient;
     
 	@GetMapping("/dc")
-    public String dc() {
+    public String dc() throws InterruptedException {
+		Thread.sleep(5000);
         String services = "Services1: " + discoveryClient.getServices();
         System.out.println(services);
         return services;
