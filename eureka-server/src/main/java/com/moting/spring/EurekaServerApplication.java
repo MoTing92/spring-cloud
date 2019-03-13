6b010f08a3b3fd490bf6a45ab48c5ee23c6a07dc
MoTing92 <18209202332@163.com>
2019-03-13 15:07:19 +0800

    在注册中心启动类中添加注释

--- eureka-server/src/main/java/com/moting/spring/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/moting/spring/EurekaServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
-
+	//在idea中测试使用git
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
